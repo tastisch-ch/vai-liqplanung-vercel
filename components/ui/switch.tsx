e74e3f5d5b991450b260d44226d0528dee202e24
Chrigu Richard <christoph@tastisch.ch>
2025-08-12 21:31:37 +0200

    feat(ui): set Switch checked color to #CEFF65 and matching focus ring

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -8,7 +8,9 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-vaios-primary focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-vaios-primary data-[state=unchecked]:bg-gray-200",
+      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=unchecked]:bg-gray-200",
+      // Use brand lime color when checked
+      "data-[state=checked]:bg-[#CEFF65] focus-visible:data-[state=checked]:ring-[#CEFF65]",
       className
     )}
     {...props}
