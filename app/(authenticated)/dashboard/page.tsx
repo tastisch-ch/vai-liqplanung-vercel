4062eaf10cf0fb9873cec2921dc091b6bf03cf3e
Chrigu Richard <christoph@tastisch.ch>
2025-09-25 15:35:28 +0200

    🧱 Make Kontostand chart span full width (lg:col-span-2)

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -159,8 +159,9 @@ export default function DashboardPage() {
       />
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        <SimpleBalanceChart isLoading={isLoading} points={forecastPoints} />
-        <div className="hidden lg:block" />
+        <div className="lg:col-span-2">
+          <SimpleBalanceChart isLoading={isLoading} points={forecastPoints} />
+        </div>
       </div>
 
       {/* Charts after balance chart temporarily removed */}
