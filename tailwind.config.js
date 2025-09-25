8eebd840a47e06dfd3696ac02ecc6b325745c45d
Chrigu Richard <christoph@tastisch.ch>
2025-09-25 23:38:08 +0200

    Tailwind config: include Tremor components in content; add @tailwindcss/forms; remove non-existing @tremor/tailwind dep

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@tremor/react/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -60,5 +61,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 } 
