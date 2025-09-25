b5c6cc72d834cb07d14e3be9291b57cf6d8a2aaf
Chrigu Richard <christoph@tastisch.ch>
2025-09-25 23:28:41 +0200

    Include Tremor global CSS in root layout to fix DateRangePicker/MultiSelect styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./globals-tooltip.css";
+import "@tremor/react/dist/esm/tremor.css";
 import { AuthProvider } from "@/components/auth/AuthProvider";
 
 const geistSans = Geist({
