eaf321dc2e94c9a075a04a404f69010687bcff25
Chrigu Richard <christoph@tastisch.ch>
2025-09-26 22:25:09 +0200

    🐛 Fix: move 'use client' to top in dashboard/page.tsx

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -1,5 +1,5 @@
+"use client";
 import PageHeader from "@/components/layout/PageHeader";
-'use client';
 
 import { useState, useEffect, useMemo } from 'react';
 import { addMonths, addDays, endOfMonth, startOfDay, startOfMonth, subMonths } from 'date-fns';
