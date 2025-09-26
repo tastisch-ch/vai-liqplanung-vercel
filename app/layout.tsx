745a733528ee34f52d0c01f3039972c5ae42c9a1
Chrigu Richard <christoph@tastisch.ch>
2025-09-26 10:25:16 +0200

    💄 UI: import Tremor CSS globally to style inputs/components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@tremor/react/dist/esm/tremor.css";
 import "./globals-tooltip.css";
 import { AuthProvider } from "@/components/auth/AuthProvider";
 
